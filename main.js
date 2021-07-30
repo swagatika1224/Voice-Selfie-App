1d58937ea62510cc4361940fad0275cc59253ea8
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-07-29 17:46:31 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -12,16 +12,24 @@ console.log(event);
 var Content = event.results[0][0].transcript;
 document.getElementById("textbox").innerHTML = Content;
 console.log(Content);
+if(Content =="take my selfie") {
+console.log("taking selfie in 5 seconds")
 speak();
-}
+    }
+ }
 
 function speak() {
 var synth = window.speechSynthesis;
-speak_data = document.getElementById("textbox").value;
+speak_data = "Taking your Selfie in 5 seconds";
 var utterThis = new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
-}
 Webcam.attach(camera);
+setTimeout(function() {
+take_snapshot();
+save();
+},5000);
+}
+
 
 Webcam.set({
 width:360,
@@ -30,3 +38,16 @@ image_format:'png',
 png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot() {
+Webcam.snap(function(data_uri) {
+document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+ }
+
+function save() {
+link = document.getElementById("link");
+image = document.getElementById("selfie_image").src;
+link.href = image;
+link.click();
+}
